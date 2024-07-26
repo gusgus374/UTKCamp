8fcfbec8fee67351397056ca98a8980925ca40f5
gusgus374 <gusalvarez374@gmail.com>
2024-07-26 11:07:03 -0400

    lucas update

---
+++ media/Lucas.py
@@ -0,0 +1,14 @@
+# write code below!
+import streamlit as st
+
+st.title("Manchesters Blue")
+
+if st.button("lucas goal"):
+    st.video("./media/lucasgoal.mp4")
+
+if st.button("lucas banger"):
+    st.video("./media/lucasbanger.mp4")
+if st.button("lucas clip"):
+    st.video("./media/lucasclip.mp4")
+st.image("./media/lucas-downey.jpeg",width=200)
+st.write("I really enjoyed using the new technology and playing soccer with new people")
--- roster/Lucas.py
+++ roster/Lucas.py
@@ -1,19 +1,19 @@
 # write code below!
 import streamlit as st
 
+import streamlit as st
+
 st.title("Manchesters Blue")
-
-my_vid = st.file_uploader("sigma")
-
-st.video(my_vid)
 
 if st.button("lucas goal"):
     st.video("./media/lucasgoal.mp4")
-    
+
 if st.button("lucas banger"):
     st.video("./media/lucasbanger.mp4")
 if st.button("lucas clip"):
     st.video("./media/lucasclip.mp4")
+st.image("./media/lucas-downey.jpeg",width=200)
+st.write("I really enjoyed using the new technology and playing soccer with new people")
 
 with st.expander("Show Lucas's code"):
     st.code(
@@ -22,17 +22,15 @@ import streamlit as st
 
 st.title("Manchesters Blue")
 
-my_vid = st.file_uploader("sigma")
-
-st.video(my_vid)
-
 if st.button("lucas goal"):
     st.video("./media/lucasgoal.mp4")
-    
+
 if st.button("lucas banger"):
     st.video("./media/lucasbanger.mp4")
 if st.button("lucas clip"):
     st.video("./media/lucasclip.mp4")
+st.image("./media/lucas-downey.jpeg",width=200)
+st.write("I really enjoyed using the new technology and playing soccer with new people")
         ''',
         language="python",
         line_numbers=True
